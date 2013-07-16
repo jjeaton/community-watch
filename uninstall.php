1d13dad54a5df0914420b8141ac16d68402e46c6
Josh Eaton <jjeaton@gmail.com>
2013-07-16 18:13:18 -0400

    Add uninstall routines

--- uninstall.php
+++ uninstall.php
@@ -14,5 +14,17 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
+// Include the plugin class and fire up our plugin
+require_once( plugin_dir_path( __FILE__ ) . 'class-community-watch.php' );
+$cw = CommunityWatch::get_instance();
+
 // Delete the options
 delete_option( 'cw_display' );
+
+// Delete the posts
+global $wpdb;
+$wpdb->query( $wpdb->prepare( "DELETE FROM $wpdb->posts WHERE post_type = %s", $cw::post_type ) );
+$wpdb->query( "DELETE FROM $wpdb->postmeta WHERE meta_key LIKE '\_cw\_reported\_%'" );
+// delete bot user
+require_once(ABSPATH . 'wp-admin/includes/user.php');
+wp_delete_user( get_bot()->ID );
